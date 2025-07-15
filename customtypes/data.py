c5685a36bc45ed2fc0b328eb76be9fc8c04b6ddc
2Echoes <floric.slimani@live.fr>
2025-07-15 17:16:59 +0200

    new custom type : Pipeline parameters

--- customtypes/data.py
+++ customtypes/data.py
@@ -2,6 +2,8 @@ from numpy import ndarray
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from typing import TypedDict
+from typing import Tuple, List, Dict
+from pydantic import BaseModel #Runtime type check
 
 class Calibration(TypedDict) :
     x_fit : LinearRegression
@@ -17,3 +19,45 @@ class Calibration(TypedDict) :
     reference_wavelength : int
     corrected_wavelength : int
     timestamp : str
+
+class PipelineParameters(BaseModel) :
+    
+    #Global
+    RUN_PATH : str
+    VOXEL_SIZE : Tuple[int,int,int]
+    WASHOUT_KEY_WORD : str
+    HAS_BEAD_CHANNEL : bool
+
+    #Input
+    MAP_FILENAME  : str
+    CYCLE_KEY : str
+    GENES_NAMES_KEY : List[str]
+    WAVELENGTH_LIST : List[int]
+    cycle_regex : str
+    FOLDER_KEYS : Dict[str,str]
+
+    #Segmentation
+    MODEL_DICT : Dict[str,str]
+    OBJECT_SIZE_DICT : Dict[str,str]
+    PLOT_VISUALS : bool
+
+    #Detection
+    detection_MAX_WORKERS : int
+    SPOT_SIZE : Tuple[int,int,int]
+    ALPHA : float
+    BETA : float
+    GAMMA : float
+    CLUSTER_SIZE : int
+    MIN_SPOT_PER_CLUSTER : int
+    ARTIFACT_RADIUS : int
+    DETECTION_SLICE_TO_REMOVE : List[int]
+
+    #Drift
+    SAVE_PATH : str
+    BEAD_SIZE : Tuple[int,int,int]
+    DRIFT_SLICE_TO_REMOVE : List[int]
+
+    #Quantification
+    COLOC_DISTANCE : int
+    quantif_MAX_WORKERS : int
+    COLOC_POPULATION : Tuple[str, ...]
