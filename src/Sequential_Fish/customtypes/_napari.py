4d9611798b82e24d1a4b447e334351ae7d9732e4
2Echoes <floric.slimani@live.fr>
2026-03-24 14:37:54 +0100

    init _napari

---
+++ src/Sequential_Fish/customtypes/_napari.py
@@ -0,0 +1,57 @@
+"""
+Super class for napari usage.
+"""
+from napari.components import ViewerModel
+from magicgui.widgets import FunctionGui
+from abc import ABC, abstractmethod
+from typing import Any
+
+class NapariWidget(ABC) :
+    """
+    Common super class for custom widgets added to napari interface during run
+    Each sub class as a specific function, but the widget can be acess with attribute .widget
+    """
+    def __init__(self):
+        self.widget = self._create_widget()
+        self.widgets = []
+        self.register_widget(self.widget)
+
+    @abstractmethod
+    def _create_widget(self) -> FunctionGui:
+        """
+        This should return a widget you can add to the napari (QWidget)
+        """
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return None
+
+    def register_widget(self, attr) :
+        self.widgets.append(attr)
+
+    def get_widgets(self) -> 'list[FunctionGui]' :
+        return self.widgets
+    
+    def disable_widget(self) :
+        for w in self.widgets :
+            w.enabled = False
+
+class NapariWizard(ABC) :
+    """
+    Commong super class for wizards to launch only when viewer is open in the context of an organoid run.
+    """
+    def __init__(self, napari_window : ViewerModel) -> None:
+        pass
+
+    @abstractmethod
+    def start_listening(
+        self, 
+        ) :
+        """
+        Principal action of widget is launched here.
+        """
+
+
+class UserInputError(Exception) :
+    """
+    Exception raised when a widget cannot function due to an error in its usage by the user.
+    """
