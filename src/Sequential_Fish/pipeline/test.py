8b56b1c991650e4ad65cc6cb1706fa50dd1ed4fc
SlimaniF <floric.slimani@live.fr>
2026-05-18 10:36:19 +0200

    wip test ome files

--- src/Sequential_Fish/pipeline/test.py
+++ src/Sequential_Fish/pipeline/test.py
@@ -4,28 +4,49 @@ from ome_types.model import TiffData
 import tifffile
 import numpy as np
 from tqdm import tqdm
+import uuid
 
-RUN_PATH = "/media/SSD_floricslimani/Fish_seq/POLR2/2026-04-09 - HeLa-Puro_POLR2_Run18/FISH_Z-stacks"
-output_path = os.path.join(RUN_PATH, "Location-01")
+RUN_PATH = "/home/floric/Documents/SeqfFish/2026-03-24 - HeLa_POLR2_Run17_tiff/FISH_Z-stacks/"
+output_path = os.path.join(RUN_PATH, "output")
 filename = "img000_000_000000_0000000000.ome.tif"
 os.makedirs(output_path, exist_ok=True)
 
 
 print("Opening image...")
-file_path_main = os.path.join(RUN_PATH, "Location-01", "img001_000_000000_0000000000.ome.tif")
-file_path_other = os.path.join(RUN_PATH, "Location-01", "img000_000_000000_0000000000.ome.tif")
+file_path_main = os.path.join(RUN_PATH, "Location-01", "img000_000_000000_0000000000.ome.tif")
+file_path_other = os.path.join(RUN_PATH, "Location-01", "img001_000_000000_0000000000.ome.tif")
 
 with tifffile.TiffFile(file_path_other) as tif :
-    metadata = ome_types.from_xml(tif.pages[0].description)
     axes_order = tif.series[0].axes
     photometric = tif.pages[0].photometric
     compression = tif.pages[0].compression
     dtype = tif.pages[0].dtype
 
+    print("\nOther")
+    print("axes order : ",axes_order)
+    print("photometric : ",photometric)
+    print("compression : ",compression)
+    print("dtype : ",dtype)
+
 with tifffile.TiffFile(file_path_main) as tif :
+    metadata = ome_types.from_xml(tif.pages[0].description)
+    main_axes_order = tif.series[0].axes
+    main_dtype = tif.pages[0].dtype
+    main_photometric = tif.pages[0].photometric
+    main_compression = tif.pages[0].compression
+    image_number = len(tif.pages)
+
+    print("\nMain")
+    print("axes order : ",main_axes_order)
+    print("photometric : ",main_photometric)
+    print("compression : ",main_compression)
+    print("dtype : ",main_dtype)
+    print("image number : ", image_number)
+
     image_stack = tif.asarray()
 
-
+cycle_number, *stack_shape = image_stack.shape
+print("stack shape : ", stack_shape)
 
 print(f"\nFILENAME : {filename}")
 print("images len : ", len(metadata.images))
@@ -48,27 +69,39 @@ if len(metadata.images) > 0 :
 
 if cycle_number == 16 :
     print(f"found {cycle_number} cycles. Adding an empty cycle")
+    empty_cycle = np.zeros(shape=(image_number, stack_shape[-2], stack_shape[-1]), dtype= image_stack.dtype)
     new_filename = filename.replace("img000", f"img0{cycle_number}")
     with tifffile.TiffWriter(os.path.join(output_path, new_filename), bigtiff=True) as tif :
         
+        tiff_uuid = f'urn:uuid:{uuid.uuid4()}'
         metadata.images[0].pixels.tiff_data_blocks.append(
             TiffData(
-                first_c = 0,
-                first_z = 0,
                 first_t = cycle_number +1,
                 plane_count = len(empty_cycle),
                 uuid=TiffData.UUID(value=tiff_uuid, file_name=filename)
             )
         )
 
-        for plane in tqdm(empty_cycle, desc="Writing planes") :
-            tif.write(
-                plane,
+        print(f"Writing new_cycle of shape : {empty_cycle.shape} with order : {axes_order}")
+        tif.write(
+                empty_cycle,
                 photometric=photometric,
-                metadata={'axes' : axes_order},
+                metadata={'axes' : "ZYX"},
                 dtype=dtype,
                 compression=compression
             )
+
+    with tifffile.TiffWriter(os.path.join(output_path, filename), bigtiff=True) :
+        print(f"Writing main tiff of shape : {image_stack[0].shape} with order : {main_axes_order}")
+        tif.write(
+            image_stack[0],
+            photometric=main_photometric,
+            metadata={"axes" : main_axes_order},
+            dtype = main_dtype,
+            compression = main_compression,
+            description = ome_types.to_xml(metadata)
+        )
+
 
 else :
     print(f"found {cycle_number} cycles. Leaving")
