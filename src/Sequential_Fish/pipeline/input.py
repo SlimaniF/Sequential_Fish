14e3baa66b79000134f2040177d7ef4f2c6e834c
SlimaniF <floric.slimani@live.fr>
2026-07-07 11:28:36 +0200

    assert folder integrity : removed old dapi stack integrity checks

--- src/Sequential_Fish/pipeline/input.py
+++ src/Sequential_Fish/pipeline/input.py
@@ -298,51 +298,37 @@ class UnevenFileNumber(InputIntegrityError) :
     """
     pass
 
-def _lvl1(RUN_PATH:str, nucleus_folder, fish_folder) :
+def _lvl1(RUN_PATH:str, fish_folder) :
     """
     returns True if ok else raise FileNotFoundError
     """
 
     dirlist = os.listdir(RUN_PATH)
     if not fish_folder in dirlist : raise FileNotFoundError("{0} folder not found in run folder.".format(fish_folder))
-    if not nucleus_folder in dirlist : raise FileNotFoundError("{0} folder not found in run folder.".format(nucleus_folder))
 
     return True
 
-def _lvl2(RUN_PATH:str, nucleus_folder, fish_folder) :
+def _lvl2(
+    RUN_PATH:str, 
+    location_key_word : str,
+    fish_folder
+    ) :
     """
     returns locations list of ok else raise ValueError
     """
     Cy3_dirlist = os.listdir(RUN_PATH + "/{0}".format(fish_folder))
-    Dapi_dirlist = os.listdir(RUN_PATH + "/{0}".format(nucleus_folder))
 
     locations_Cy3 = []
-    locations_Dapi = []
 
     for file in Cy3_dirlist :
-        if 'Location-' in file : 
+        if location_key_word in file : 
             locations_Cy3.append(file)
-
-    for file in Dapi_dirlist :
-        if 'Location-' in file : 
-            locations_Dapi.append(file)
-
     locations_Cy3.sort()
-    locations_Dapi.sort()
 
-    if locations_Cy3 != locations_Dapi :
-        raise ValueError("Missmatch between locations found in Cy3 folder and in Dapi folder")
-    else :
-        return locations_Cy3
+    return locations_Cy3
 
-def _lvl3(RUN_PATH, locations, fish_folder, nucleus_folder) :
+def _lvl3(RUN_PATH, locations, fish_folder) :
 
-    #Dapi
-    for location in locations :
-        dirlist = os.listdir(RUN_PATH + '/{0}/'.format(nucleus_folder) + location)
-        if len(dirlist) == 0 : raise FileNotFoundError("Dapi acquisition not found for location : {0}".format(location))
-    
-    #Cy3
     file_number = []
     file_dict = {}
     for location in locations :
@@ -357,9 +343,9 @@ def _lvl3(RUN_PATH, locations, fish_folder, nucleus_folder) :
 
     return file_dict
 
-def assert_run_folder_integrity(run_path, fish_folder, nucleus_folder) :
-    _lvl1(run_path, fish_folder=fish_folder, nucleus_folder=nucleus_folder)
-    locations_list = _lvl2(run_path, fish_folder=fish_folder, nucleus_folder=nucleus_folder)
-    file_dict = _lvl3(run_path, locations_list, fish_folder=fish_folder, nucleus_folder=nucleus_folder)
+def assert_run_folder_integrity(run_path, fish_folder, location_key_word) :
+    _lvl1(run_path, fish_folder=fish_folder)
+    locations_list = _lvl2(run_path, fish_folder=fish_folder, location_key_word=location_key_word)
+    file_dict = _lvl3(run_path, locations_list, fish_folder=fish_folder)
 
     return file_dict
