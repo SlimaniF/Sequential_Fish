502627be48b38887234c484d14422e1defaf0149
2Echoes <floric.slimani@live.fr>
2026-07-28 14:27:02 +0200

    version incremented

--- src/Sequential_Fish/viewer/__init__.py
+++ src/Sequential_Fish/viewer/__init__.py
@@ -3,6 +3,8 @@ Entry point to launch Sequential Fish viewer
 """
 from .main import main
 
+import logging
+
 from .load import initiate_load_widgets
 from . analysis import initiate_analysis_widgets
 from . locations import initiate_location_widgets
@@ -13,4 +15,10 @@ from .utils import update_layer_from_LayerDataTuple
 
 
 def run(run_path : str) :
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[logging.StreamHandler()]
+    )
+    logging.info("Starting viewer module")
     main(run_path)
